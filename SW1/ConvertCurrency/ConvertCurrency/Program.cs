611271ab6499d363cca0661768d645479a99952c
My PC <My PC@DESKTOP-9PVVVC5>
2025-09-15 10:15:09 +0800

    updated SW1

---
+++ SW1/ConvertCurrency/ConvertCurrency/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertCurrency
+{
+   
+    class Program
+    {
+        static (double peso, double yen) ConvertCurrency(double dollar)
+        {
+            double peso = dollar * 57.17;  
+            double yen = dollar * 146.67;     
+            return (peso, yen); 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency in ($): ");
+            double dollar = double.Parse(Console.ReadLine());
+            
+            var result = ConvertCurrency(dollar);
+
+          
+            Console.WriteLine(" Dollar($)  Phil Peso(P)  Jpn Yen(Y)");
+            Console.WriteLine($"    {dollar}        {result.peso}        {result.yen}");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ SW1/PrimeComposite/SW_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW_1
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+
+            string[] parts = input.Split(' ');
+
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                int number = int.Parse(parts[i]);
+                if (IsPrime(number))
+                    Console.WriteLine($"{i + 1}.  {number} Prime");
+                else
+                    Console.WriteLine($"{i + 1}.  {number} Composite");
+                
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
